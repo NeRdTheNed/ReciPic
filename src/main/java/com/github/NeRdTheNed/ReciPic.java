081d3100cc38c5d5f2153181c0fa27170bed5145
Ned Loynd <41816363+NeRdTheNed@users.noreply.github.com>
2021-02-10 12:05:21 +1100

    Create basic Forge mod structure

---
+++ src/main/java/com/github/NeRdTheNed/ReciPic.java
@@ -0,0 +1,13 @@
+package com.github.NeRdTheNed;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "recipic")
+public class ReciPic {
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        // TODO mod
+    }
+}
