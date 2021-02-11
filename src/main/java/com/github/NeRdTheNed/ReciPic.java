8747a8ac0caf125582ab0b0521d28acebaeede2f
Ned Loynd <41816363+NeRdTheNed@users.noreply.github.com>
2021-02-11 16:07:46 +1100

    Import WildcardItem from my fork of ARG, start work on mocking up configuration GUI
    Nobody told me that buttons are this weird in Forge!
    
    Side note: the WildcardIcon item and textures were made entirely by me, so they don’t fall under ARG's license.

--- src/main/java/com/github/NeRdTheNed/ReciPic.java
+++ src/main/java/com/github/NeRdTheNed/ReciPic.java
@@ -1,13 +1,70 @@
 package com.github.NeRdTheNed;
 
+import java.util.Set;
+
+import cpw.mods.fml.client.IModGuiFactory;
+import cpw.mods.fml.client.event.ConfigChangedEvent.OnConfigChangedEvent;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
 
-@Mod(modid = "recipic")
-public class ReciPic {
+@Mod(modid = ReciPic.MOD_ID, guiFactory = "com.github.NeRdTheNed.ReciPic")
+public final class ReciPic implements IModGuiFactory {
+
+    public static Configuration config;
+
+    public static boolean displaySingleOreDictEntries;
+
+    public static final String MOD_ID = "ReciPic";
+
+    public static final Item wildcardItem = new WildcardItem();
+
+    private static void syncConfig() {
+        displaySingleOreDictEntries = config.get(Configuration.CATEGORY_GENERAL, "ReciPic.displaySingleOreDictEntries", false).getBoolean();
+        config.save();
+    }
+
+    @SubscribeEvent
+    public void confChange(OnConfigChangedEvent event) {
+        if (event.modID.equals(MOD_ID)) {
+            syncConfig();
+        }
+    }
+
+    @Override
+    public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement a) {
+        return null;
+    }
+
     @EventHandler
-    public void init(FMLInitializationEvent event) {
-        // TODO mod
+    public void init(FMLPreInitializationEvent event) {
+        config = new Configuration(event.getSuggestedConfigurationFile());
+        syncConfig();
+        MinecraftForge.EVENT_BUS.register(this);
+        FMLCommonHandler.instance().bus().register(this);
+        GameRegistry.registerItem(wildcardItem, "WildcardItem");
+    }
+
+    @Override
+    public void initialize(Minecraft a) {
+        /* This space left intentionally blank */
+    }
+
+    @Override
+    public Class<? extends GuiScreen> mainConfigGuiClass() {
+        return ReciPicConfig.class;
+    }
+
+    @Override
+    public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
+        return null;
     }
 }
---
+++ src/main/java/com/github/NeRdTheNed/WildcardItem.java
@@ -0,0 +1,46 @@
+package com.github.NeRdTheNed;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.Item;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
+
+public final class WildcardItem extends Item {
+
+    private static final String baseWildcardItemName = "WildcardItem";
+
+    private static final String baseWildcardItemTextureName = ReciPic.MOD_ID + ":" + baseWildcardItemName;
+    private static final String unlocalisedWildcardItemName = ReciPic.MOD_ID + "." + baseWildcardItemName;
+
+    private IIcon[] iconArray;
+
+    public WildcardItem() {
+        setUnlocalizedName(unlocalisedWildcardItemName);
+        setMaxStackSize(1);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIconFromDamage(int damage) {
+        if (damage == 0) {
+            return itemIcon;
+        } else {
+            return iconArray[MathHelper.clamp_int(damage, 1, 9)];
+        }
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iiconRegister) {
+        itemIcon = iiconRegister.registerIcon(baseWildcardItemTextureName + "Unique");
+        iconArray = new IIcon[10];
+        iconArray[0] = itemIcon;
+
+        for (int i = 1; i <= 9; i++) {
+            iconArray[i] = iiconRegister.registerIcon(baseWildcardItemTextureName + i);
+        }
+    }
+
+}
